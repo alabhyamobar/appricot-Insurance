87c3193889623f7d76f86bbe15bc033096de2c01
alabhyamobar <156466596+alabhyamobar@users.noreply.github.com>
2025-05-25 10:55:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/appricot-Insurance/",  // Correct format for GitHub Pages
+  base: "/appricot-Insurance/", // IMPORTANT for GitHub Pages
 })
