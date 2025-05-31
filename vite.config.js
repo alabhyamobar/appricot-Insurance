cfac4de5ee069eabdca2cbdef7e55cb73f4d30e8
alabhyamobar <156466596+alabhyamobar@users.noreply.github.com>
2025-05-31 15:03:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/appricot-Insurance/', // <- must match your repo name
+  base: '/', // <- must match your repo name
   plugins: [
     tailwindcss(),
   ],
